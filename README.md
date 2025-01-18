c0625cda0a47cd5a26dd252c0ebe9b313fee49b7
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2025-01-18 18:28:49 -0300

    :recycle: refactor: Rename functions search_people to search_people_and_connect

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
-# Automacao Linkedin Para Nova Conexao
+### Automacao Linkedin Para Nova Conexao
 
+Techs 
 
-Tempo: 3,5
+python 
+selenium
+webdriver-manager
 
-[x] 1. Acessar linkedin.
+Requiriments: 
+
+ - You must already be register to linkedin
+
+Use: 
+
+    DESCRIPTION="tech recruiter" # Data searched
+
+    USER_LINKEDIN='' # User of linkedin from access
+    PASSWORD_LINKEDIN='' # User password from access
+
+    LIMIT_CONNECTIONS=30 Quantity of people app most be connect
+    
+
+Tempo de produção: 5 Horas
+
+[x] 1. Acessar linkedin pagina web web.
 [x] 2. Preencher senha e usuário.
 [x] 3. Pesquisar sobre descrição de recrutador.
 [x] 4. Clicar no botão (Pessoas).
 [x] 5. Acessar todos os contatos e clicar no botão (Conectar).
-    [] 5.1. Verificar o numero total de paginas
-    [] 5.2. Realizar isso página por pagina, até expiração de conexões
+    [x] 5.1. Realizar isso página por pagina, até expiração de conexões.
 
 
 
--- __init__.py
+++ __init__.py
@@ -11,15 +11,12 @@ from selenium.common.exceptions import WebDriverException
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import InvalidSelectorException
 
 from connect_people.do_login.do_login import do_login
-from connect_people.encode_message_for_url.encode_message_for_url import encode_message_for_url
-from connect_people.search_people.search_people import search_people
+from connect_people.search_people_and_connect.search_people_and_connect import search_people_and_connect
 from utils.logging.log_manager.log_manager import write_to_log
-from utils.wait_for_element_load.wait_for_element_load import wait_for_element_load
-# from utils.move_to_file.move_to_file import move_to_file
+# from utils.wait_for_element_load.wait_for_element_load import wait_for_element_load
 
 
 
@@ -40,24 +37,14 @@ if __name__ == '__main__':
             'password': os.getenv("PASSWORD_LINKEDIN")
         }
         do_login(driver, user_login)
-        # wait_for_element_load()
         search_data_people = {
             'description': os.getenv("DESCRIPTION"),
             'location': os.getenv("LOCATION")
         }
-        sleep(7)
+        sleep(12)
         
-        # description_encoded = encode_message_for_url(description_people)
-            
-        # driver.get(f'https://www.linkedin.com/search/results/people/?keywords=TECH&location=Sao%20Paulo')
-
-        search_people(driver, search_data_people)
+        search_people_and_connect(driver, search_data_people)
    
-        
-        # path_file_imports = os.path.join(os.getcwd(), 'import', 'jobs.csv')
-        # path_file_export = os.path.join(os.getcwd(),'exported', 'datetime_now' +'.csv')
-        
-        # move_to_file(path_file_imports, path_file_export)
     except WebDriverException as e:
         write_to_log(f"WebDriverException: {traceback.format_exc()}", type='error')
     except InvalidSelectorException as e:
